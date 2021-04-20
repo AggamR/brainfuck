dfba8eaa5aa954732f0732215ab748e5f16d0b59
Aggam Rahamim <agamclass@gmail.com>
2021-04-20 19:53:26 +0300

    basic functionality for reading from files. NO EROOR MESSAGES FOR NON-EXISTING FILE: TO CHNAGE.

--- README.md
+++ README.md
@@ -2,6 +2,4 @@
 This is a brainfuck interpreter written in C. it is far from complete, but I've got *something* working, so I might as well upload it. My first project in C ever, besides a small program I wrote to find anti-prime numbers.
 
 ## what doesn't work:
-* input from files (low priority)
-* some programs for some reason (see hello world from [here](https://gist.github.com/roachhd/dce54bec8ba55fb17d3a)).
-* errors n' stuff. 
+* errors n' stuff (add in a non-minimal compiling option).
--- main.c
+++ main.c
@@ -1,13 +1,26 @@
 #include <stdio.h>
 #include <string.h>
 
-int main() {
-    // (emulated) memory stuff
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        printf("usage: `bf FILE`");
+        return 1;
+    }
+
+    FILE * fpointer;
+    fpointer = fopen("test.txt", "r"); 
+    char line[128];
+    char code[512];
+    while (!feof(fpointer)) {
+        fgets(line, 128, fpointer);
+        strcat(code, line);
+    }
+    fclose(fpointer); 
+
     int memp = 0; // memory pointer
     int memSize = 64;
     int mem[memSize];
 
-    // stack
     int stack[memSize/4];
     int sp = memSize/4 - 1; // stack pointer
 
@@ -16,9 +29,6 @@ int main() {
         mem[i] = 0;
         stack[i/4] = 0;  // must be a netter way of doing this...
     }
-        
-
-    char code[] = ">+++++++++[<++++++++>-]<.>+++++++[<++++>-]<+.+++++++..+++.[-]>++++++++[<++++>-] <.>+++++++++++[<++++++++>-]<-.--------.+++.------.--------.[-]>++++++++[<++++>- ]<+.[-]++++++++++.";
 
     // loop chars in code
     for (int i = 0; i < strlen(code); i++) {
