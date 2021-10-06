68bd7bc91cdd4079efeb9ea2462edc6af5fb95f3
Aggam R <agamclass@gmail.com>
2021-10-06 18:42:08 +0300

    expanded memory and stack as well as max file size. Added so bracket will not be intered if current place in memory isnt 0. Curently Solving Bugs.

--- main.c
+++ main.c
@@ -2,12 +2,13 @@
 #include <string.h>
 #include <stdlib.h>
 
-#define COLOR_RED     "\x1b[31m"
-#define COLOR_RESET   "\x1b[0m"
+#define COLOR_RED       "\x1b[31m"
+#define COLOR_RESET     "\x1b[0m"
+#define MEM_SIZE        4096
 
 int main(int argc, char *argv[]) {
     if (argc != 2) {
-        printf("usage: `bf FILE`");
+        printf("usage: `bf FILE`\n");
         return 1;
     }
 
@@ -15,30 +16,30 @@ int main(int argc, char *argv[]) {
     fpointer = fopen(argv[1], "r"); 
 
     if (fpointer == NULL) {
-        printf(COLOR_RED "file can't be opened\n" COLOR_RESET);
+        printf("file can't be opened\n");
         return 1;
     }
 
-    char line[4096];
-    char code[16000]; 
+    char line[MEM_SIZE];  // memsize was chosen at random
+    char code[MEM_SIZE*4]; 
 
     while (!feof(fpointer)) {
-        fgets(line, 4096, fpointer);
+        fgets(line, MEM_SIZE, fpointer);
         strcat(code, line);
     }
     fclose(fpointer); 
 
     int memp = 0; // memory pointer
-    int memSize = 4096;
-    int mem[memSize];
-
-    int stack[memSize/4];
-    int sp = memSize/4 - 1; // stack pointer
+    unsigned char mem[MEM_SIZE];
+    int stack[MEM_SIZE/4];
+    int sp = MEM_SIZE/4 - 1; // stack pointer
+    
+    int bCount = 0; // bracket count 
 
     char iscomment = 0;
 
     // reset arrays
-    for (int i = 0; i < memSize; i++) {
+    for (int i = 0; i < MEM_SIZE; i++) {
         mem[i] = 0;
         stack[i/4] = 0;  // must be a netter way of doing this...
     }
@@ -53,7 +54,6 @@ int main(int argc, char *argv[]) {
 
         if (iscomment) continue;
 
-        //printf("\nmem[memp]: %d\ni: %d", mem[memp], i);
         switch (code[i]) {
             case '#':
                 iscomment = 1;
@@ -65,7 +65,7 @@ int main(int argc, char *argv[]) {
                 mem[memp]--;
                 break;
             case '>':
-                if (memp < memSize - 1) memp++;
+                if (memp < MEM_SIZE - 1) memp++;
                 break;
             case '<':
                 if (memp > 0) memp--;
@@ -77,13 +77,27 @@ int main(int argc, char *argv[]) {
                 mem[memp] = getchar();
                 break;
             case '[':
-                stack[sp] = i;
-                sp++;
+                // if not 0, enter
+                if (mem[memp] != 0) {
+                    stack[sp] = i;
+                    sp++;
+                } else {
+                    bCount = 0;
+                    for (int j = i+1; j < MEM_SIZE; j++) {
+                        if (code[j] == ']')
+                            if (bCount == 0) {
+                                i = j;
+                                break;
+                            } else bCount--;
+
+                        else if (code[j] == '[')
+                            bCount++; 
+                    }
+                }
                 break;
             case ']':
                 if (mem[memp] != 0) {
                     if (i - 1 == stack[sp-1]) return 0;
-
                     i = stack[sp-1];
                 } else sp--;
                 break;
